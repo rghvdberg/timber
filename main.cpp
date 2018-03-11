af2f2b2dbb3b0043c653b24e684fc1ee6df76695
Rob van den Berg <rghvdberg@gmail.com>
2018-03-12 00:19:50 +0100

    page 82 ; resized graphics

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace sf;
 int main()
 {
 	// Create a video mode object
-	VideoMode vm(1920, 1080);
+	VideoMode vm(1280, 800);
 
 	// Create and open a window for the game
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
@@ -26,6 +26,58 @@ int main()
 
 	// Set the spriteBackground to cover the screen
 	spriteBackground.setPosition(0, 0);
+
+	// Make a tree sprite
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(1280/2, 0);
+
+	// Prepare the bee
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 300);
+
+	// Is the bee currently moving?
+	bool beeActive = false;
+
+	// How fast can the bee fly
+	float beeSpeed = 0.0f;
+
+	// make 3 cloud sprites from 1 texture
+	Texture textureCloud;
+
+	// Load 1 new texture
+	textureCloud.loadFromFile("graphics/cloud.png");
+
+	// 3 New sprites with the same texture
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+
+	// Position the clouds off screen
+	spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 100);
+    spriteCloud3.setPosition(0, 200);
+
+    // Are the clouds currently on screen?
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+
+    // How fast is each cloud?
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+
+    Clock clock;
+
 
 
 	while (window.isOpen())
@@ -48,6 +100,23 @@ int main()
 		****************************************
 		*/
 
+		// Measure time
+		Time dt = clock.restart();
+
+		// Setup the bee
+		if (!beeActive)
+        {
+            // How fast is the bee
+            srand((int)time(0));
+            beeSpeed = (rand() % 200) - 200;
+
+            // How high is the bee
+            srand((int)time(0) * 10);
+            float height = (rand() % 500) + 500;
+            spriteBee.setPosition(1300, height);
+            beeActive = true;
+        }
+
 
 		/*
 		****************************************
@@ -60,6 +129,17 @@ int main()
 
 		// Draw our game scene here
 		window.draw(spriteBackground);
+
+		// Draw the clouds
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+
+        // Draw the tree
+        window.draw(spriteTree);
+
+        // Draw the insect
+        window.draw(spriteBee);
 
 		// Show everything we just drew
 		window.display();
